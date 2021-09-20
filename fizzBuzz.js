e7d81b5d200d677deb5f9e894875160d492359b0
Alan Sobenes <alansobenes@gmail.com>
2021-09-19 17:50:59 -0700

    FizzBuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,13 +1,15 @@
-for(var i=0; i<=100; i++){
-    if (i%3==0 && i%5==0){
+for(var i=1; i<=100; i++){
+    if ((i%3==0) && (i%5==0)){
         console.log("FizzBuzz")
     }
-    if (i%3===0){
+    else if (i%3===0){
         console.log("Fizz");
     }
-    if (i%5===0){
+    else if (i%5===0){
         console.log("Buzz")
     }
+    else console.log(i);
     
 }
+
 
